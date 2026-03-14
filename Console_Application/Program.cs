fab3d5c9e020fd6342ea02e8c8d3abd62ea6f719
Kurek Sebastian <s34366@pjwstk.edu.pl>
2026-03-14 12:43:04 +0100

    Added minValue variable to the program.cs

--- Console_Application/Program.cs
+++ Console_Application/Program.cs
@@ -5,6 +5,7 @@ int[] values = { 1, 2, 3, 4, 5 };
 
 int maxValue = StatisticsHelper.CalculateMax(values);
 int averageValue = (int)StatisticsHelper.CalculateAverage(values);
+int minValue = StatisticsHelper.GetMax(values);
 
 Console.WriteLine($"The maximum value is: {maxValue}");
 Console.WriteLine($"Average of values is: {averageValue} ");
