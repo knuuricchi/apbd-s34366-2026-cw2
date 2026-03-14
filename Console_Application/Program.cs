bf0175a0df1c42e3de2c0ef0cb4618de7d8ba304
Kurek Sebastian <s34366@pjwstk.edu.pl>
2026-03-14 12:16:32 +0100

    Remove 'Hello World' from program.cs

--- Console_Application/Program.cs
+++ Console_Application/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Console_Application;
 
-Console.WriteLine("Hello, World!");
 int[] values = { 1, 2, 3, 4, 5 };
 int maxValue = StatisticsHelper.CalculateMax(values);
 Console.WriteLine($"The maximum value is: {maxValue}");
